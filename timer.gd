448a73afddd88ec3d26b11eb1e005a1f413d429d
Orrickl <orrickl@stu.otc.school.nz>
2024-06-13 12:25:50 +1200

    test

--- timer.gd
+++ timer.gd
@@ -9,12 +9,12 @@ func _process(delta):
 		text = var2str(round(time*100)/100)
 		print("hi")
 
-func _on_Start_body_entered(body):
+func _on_Start_body_entered(_body):
 	timer_on = true
 
 
 
-func _on_Finish_body_entered(body):
+func _on_Finish_body_entered(_body):
 	timer_on = false
 
 
