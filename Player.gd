c7f8d0bdc721e2a46356253f5e821f81361f7e7a
Orrickl <orrickl@stu.otc.school.nz>
2024-06-24 13:30:45 +1200

    changed from third person to first person

--- Player.gd
+++ Player.gd
@@ -11,19 +11,24 @@ export var gravity := 50.0
 var _velocity := Vector3.ZERO
 var _snap_vector := Vector3.DOWN
 
-onready var _spring_arm: SpringArm = $SpringArm
+
 onready var _model: Spatial = $PlayerShape
 
 func _physics_process(delta: float) -> void:
 	var move_direction := Vector3.ZERO
 	move_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	move_direction.z = Input.get_action_strength("back") - Input.get_action_strength("forward")
-	move_direction = move_direction.rotated(Vector3.UP, _spring_arm.rotation.y)
+	move_direction = move_direction.rotated(Vector3.UP, Camera.rotation.y)
 	
 	_velocity.x = move_direction.x * speed
 	_velocity.z = move_direction.z * speed
 	
 	_velocity.y -= gravity * delta
+	
+	if ( _velocity.z or _velocity.x != 0):
+		$PlayerArea/Run/RootNode/AnimationPlayer.play("mixamo.com")
+	
+
 
 	
 	var just_landed := is_on_floor() and _snap_vector == Vector3.ZERO
@@ -39,7 +44,21 @@ func _physics_process(delta: float) -> void:
 		var look_direction = Vector2(_velocity.z, _velocity.x)
 		_model.rotation.y = look_direction.angle()
 
-func _process(_delta: float) -> void:
-	_spring_arm.translation = translation
+export var mouse_sensitivity := 0.05
+
+
+
+func _ready() -> void:
+	set_as_toplevel(true)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotation_degrees.x -= event.relative.y * mouse_sensitivity
+		rotation_degrees.x = clamp(rotation_degrees.x, -30.0, 0.0)
+		
+		rotation_degrees.y -= event.relative.x * mouse_sensitivity
+		rotation_degrees.y = wrapf(rotation_degrees.y, 0.0, 360.0)
 
 
