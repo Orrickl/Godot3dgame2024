67e1da37572268a663d6fbd71d0df8a76eaf70cd
Orrickl <109933718+Orrickl@users.noreply.github.com>
2024-09-03 10:05:00 +1200

    dhfh

--- End_menu_post.gd
+++ End_menu_post.gd
@@ -7,17 +7,17 @@ onready var http_request = $HTTPRequest
 
 var url = "https://meowfacts.herokuapp.com/"
 
-var json = JSON.data_to_send
+#var json = JSON.data_to_send
 var headers = ["Content-Type: application/json"]
 
 
 
-func _on_end_menu_mesh_post():
-	$HTTPRequest.request(url, headers, HTTPClient.METHOD_POST, json)
+#func _on_end_menu_mesh_post():
+	#$HTTPRequest.request(url, headers, HTTPClient.METHOD_POST, json)
 
 
-func _on_HTTPRequest_request_completed(result, response_code, headers, body):
-	print(body.get_string_from_utf8())
+#func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+#	print(body.get_string_from_utf8())
 
 
 func _on_timer_lable_time(timer):
